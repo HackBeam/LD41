793de8e668579c655c7b6fe8d07f924e20137cc7
Alberto Martín <albertouma92@gmail.com>
2018-04-21 21:26:39 +0200

    Boat movement

---
+++ Assets/1. Scripts/BoatMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour {
+
+	public float speed = 20f;
+	public LayerMask dockLayer;
+
+	private void Update()
+	{
+		transform.position += Vector3.left * speed * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(dockLayer == (dockLayer | (1 << other.gameObject.layer))){
+			Destroy(gameObject);
+		}
+	}
+
+}
